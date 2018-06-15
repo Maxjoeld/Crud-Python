9a35a00dd7020ef1aadb9f6729885df997e21f58
Maximo DeLaRosa <mjd809@gmail.com>
2018-06-15 17:29:34 -0400

    Added Html/css

--- first_project/urls.py
+++ first_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include 
-from blog import views
+from django.contrib.auth import views 
+
 
 urlpatterns = [
   path('', include('blog.urls')),  
   path('admin/', admin.site.urls),
+  path('accounts/login/$', views.login, name='login'),
+  path('accounts/logout/$', views.logout, name='logout',kwargs={'next_page':'/'}),
 ]
